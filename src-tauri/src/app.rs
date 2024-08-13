b0cb3d5f1c81ad3adf4eaad7b36ea291fddddc72
Andrew Burke <nomnomab1@gmail.com>
2024-08-13 14:22:37 -0400

    Formatting tweaks

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -3,141 +3,144 @@ use std::{path::PathBuf, sync::Mutex};
 use crate::{cache, editor, errors, prefs, project};
 
 pub struct AppState {
-    pub prefs: Mutex<prefs::Prefs>,
-    pub user_cache: Mutex<cache::UserCache>,
-    pub projects: Mutex<Vec<project::Project>>,
-    pub editors: Mutex<Vec<editor::UnityEditorInstall>>,
+  pub prefs: Mutex<prefs::Prefs>,
+  pub user_cache: Mutex<cache::UserCache>,
+  pub projects: Mutex<Vec<project::Project>>,
+  pub editors: Mutex<Vec<editor::UnityEditorInstall>>,
 }
 
 pub fn get_save_path(name: &str, app_handle: &tauri::AppHandle) -> anyhow::Result<PathBuf> {
-    let config = app_handle.config();
-    let config_dir = tauri::api::path::app_config_dir(&config)
-        .expect("failed to get app config dir");
-
-    std::fs::create_dir_all(&config_dir)?;
-
-    let path = config_dir.join(name).with_extension("json");
-    // println!("{} path: {:?}", name, path);
-    Ok(path)
+  let config = app_handle.config();
+  let config_dir = tauri::api::path::app_config_dir(&config)
+    .expect("failed to get app config dir");
+  
+  std::fs::create_dir_all(&config_dir)?;
+  
+  let path = config_dir
+    .join(name)
+    .with_extension("json");
+  // println!("{} path: {:?}", name, path);
+  Ok(path)
 }
 
 pub fn load_from_disk<T>(path: impl Into<PathBuf>) -> anyhow::Result<T>
-    where T: Default + serde::Serialize + serde::de::DeserializeOwned {
-    let path = path.into();
-    let json = std::fs::read_to_string(&path) .unwrap_or("{}".to_string());
-    let (exists, field) = match serde_json::from_str(&json) {
-        Ok(field) => (true, field),
-        Err(_) => (false, T::default()),
-    };
-
-    if !exists {
-        save_to_disk::<T>(path, &field)?;
-    }
-
-    Ok(field)
+where T: Default + serde::Serialize + serde::de::DeserializeOwned {
+  let path = path.into();
+  let json = std::fs::read_to_string(&path)
+    .unwrap_or("{}".to_string());
+  let (exists, field) = match serde_json::from_str(&json) {
+    Ok(field) => (true, field),
+    Err(_) => (false, T::default()),
+  };
+  
+  if !exists {
+    save_to_disk::<T>(path, &field)?;
+  }
+  
+  Ok(field)
 }
 
 pub fn save_new_to_disk<T>(path: impl Into<PathBuf>) -> anyhow::Result<T>
-    where T: Default + serde::Serialize {
-    let value = T::default();
-    save_to_disk(path, &value)?;
-    Ok(value)
+where T: Default + serde::Serialize {
+  let value = T::default();
+  save_to_disk(path, &value)?;
+  Ok(value)
 }
 
 pub fn save_to_disk<T>(path: impl Into<PathBuf>, field: &T) -> anyhow::Result<()>
-    where T: serde::Serialize {
-    let path = path.into();
-    let json = serde_json::to_string_pretty(&field)?;
-    std::fs::write(path, json)?;
-    Ok(())
+where T: serde::Serialize {
+  let path = path.into();
+  let json = serde_json::to_string_pretty(&field)?;
+  std::fs::write(path, json)?;
+  Ok(())
 }
 
 // prefs
 
 pub fn get_prefs_save_path(app_handle: &tauri::AppHandle) -> anyhow::Result<PathBuf> {
-    let path = get_save_path("prefs", app_handle)?;
-    Ok(path)
+  let path = get_save_path("prefs", app_handle)?;
+  Ok(path)
 }
 
 pub fn get_prefs(app_state: &tauri::State<AppState>) -> anyhow::Result<prefs::Prefs> {
-    let prefs = app_state.prefs.lock()
-        .map_err(|_| errors::str_error("Failed to get prefs. Is it locked?"))?;
-
-    Ok(prefs.clone())
+  let prefs = app_state.prefs.lock()
+    .map_err(|_| errors::str_error("Failed to get prefs. Is it locked?"))?;
+  
+  Ok(prefs.clone())
 }
 
 pub fn load_prefs_from_disk(app_handle: &tauri::AppHandle) -> anyhow::Result<prefs::Prefs> {
-    let path = get_prefs_save_path(app_handle)?;
-    load_from_disk(&path)
+  let path = get_prefs_save_path(app_handle)?;
+  load_from_disk(&path)
 }
 
 pub fn save_new_prefs_to_disk(app_handle: &tauri::AppHandle) -> anyhow::Result<prefs::Prefs> {
-    let path = get_prefs_save_path(app_handle)?;
-    save_new_to_disk::<prefs::Prefs>(path)
+  let path = get_prefs_save_path(app_handle)?;
+  save_new_to_disk::<prefs::Prefs>(path)
 }
 
 pub fn save_prefs_to_disk(prefs: &prefs::Prefs, app_handle: &tauri::AppHandle) -> anyhow::Result<()> {
-    let path = get_prefs_save_path(app_handle)?;
-    save_to_disk(path, prefs)?;
-    Ok(())
+  let path = get_prefs_save_path(app_handle)?;
+  save_to_disk(path, prefs)?;
+  Ok(())
 }
 
 // user cache
 
 pub fn get_user_cache_save_path(app_handle: &tauri::AppHandle) -> anyhow::Result<PathBuf> {
-    let path = get_save_path("user_cache", app_handle)?;
-    Ok(path)
+  let path = get_save_path("user_cache", app_handle)?;
+  Ok(path)
 }
 
 pub fn get_user_cache(app_state: &tauri::State<AppState>) -> anyhow::Result<cache::UserCache> {
-    let user_cache = app_state.user_cache.lock()
-        .map_err(|_| errors::str_error("Failed to get user_cache. Is it locked?"))?;
-
-    Ok(user_cache.clone())
+  let user_cache = app_state.user_cache.lock()
+    .map_err(|_| errors::str_error("Failed to get user_cache. Is it locked?"))?;
+  
+  Ok(user_cache.clone())
 }
 
 pub fn load_user_cache_from_disk(app_handle: &tauri::AppHandle) -> anyhow::Result<cache::UserCache> {
-    let path = get_user_cache_save_path(app_handle)?;
-    load_from_disk(&path)
+  let path = get_user_cache_save_path(app_handle)?;
+  load_from_disk(&path)
 }
 
 pub fn save_new_user_cache_to_disk(app_handle: &tauri::AppHandle) -> anyhow::Result<cache::UserCache> {
-    let path = get_user_cache_save_path(app_handle)?;
-    save_new_to_disk::<cache::UserCache>(path)
+  let path = get_user_cache_save_path(app_handle)?;
+  save_new_to_disk::<cache::UserCache>(path)
 }
 
 pub fn save_user_cache_to_disk(user_cache: &cache::UserCache, app_handle: &tauri::AppHandle) -> anyhow::Result<()> {
-    let path = get_user_cache_save_path(app_handle)?;
-    save_to_disk(path, &user_cache)
+  let path = get_user_cache_save_path(app_handle)?;
+  save_to_disk(path, &user_cache)
 }
 
 // projects
 
 pub fn get_projects_save_path(app_handle: &tauri::AppHandle) -> anyhow::Result<PathBuf> {
-    let path = get_save_path("projects", app_handle)?;
-    Ok(path)
+  let path = get_save_path("projects", app_handle)?;
+  Ok(path)
 }
 
 pub fn get_projects(app_state: &tauri::State<AppState>) -> anyhow::Result<Vec<project::Project>> {
-    let projects = app_state.projects.lock()
-        .map_err(|_| errors::str_error("Failed to get projects. Is it locked?"))?;
-
-    Ok(projects.clone())
+  let projects = app_state.projects.lock()
+    .map_err(|_| errors::str_error("Failed to get projects. Is it locked?"))?;
+  
+  Ok(projects.clone())
 }
 
 pub fn load_projects_from_disk(app_handle: &tauri::AppHandle) -> anyhow::Result<Vec<project::Project>> {
-    let path = get_projects_save_path(app_handle)?;
-    load_from_disk(&path)
+  let path = get_projects_save_path(app_handle)?;
+  load_from_disk(&path)
 }
 
 pub fn save_new_projects_to_disk(app_handle: &tauri::AppHandle) -> anyhow::Result<Vec<project::Project>> {
-    let path = get_projects_save_path(app_handle)?;
-    save_new_to_disk::<Vec<project::Project>>(path)
+  let path = get_projects_save_path(app_handle)?;
+  save_new_to_disk::<Vec<project::Project>>(path)
 }
 
 pub fn save_projects_to_disk(projects: &Vec<project::Project>, app_handle: &tauri::AppHandle) -> anyhow::Result<()> {
-    let path = get_projects_save_path(app_handle)?;
-    save_to_disk(path, &projects)
+  let path = get_projects_save_path(app_handle)?;
+  save_to_disk(path, &projects)
 }
 
 // editors
@@ -148,10 +151,10 @@ pub fn save_projects_to_disk(projects: &Vec<project::Project>, app_handle: &taur
 // }
 
 pub fn get_editors(app_state: &tauri::State<AppState>) -> anyhow::Result<Vec<editor::UnityEditorInstall>> {
-    let editors = app_state.editors.lock()
-        .map_err(|_| errors::str_error("Failed to get editors. Is it locked?"))?;
-
-    Ok(editors.clone())
+  let editors = app_state.editors.lock()
+    .map_err(|_| errors::str_error("Failed to get editors. Is it locked?"))?;
+  
+  Ok(editors.clone())
 }
 
 // pub fn load_editors_from_disk(app_handle: &tauri::AppHandle) -> anyhow::Result<Vec<editor::UnityEditorInstall>> {
@@ -173,32 +176,32 @@ pub fn get_editors(app_state: &tauri::State<AppState>) -> anyhow::Result<Vec<edi
 
 #[tauri::command]
 pub fn cmd_show_path_in_file_manager(path: String) {
-    #[cfg(not(target_os = "macos"))]
-    showfile::show_path_in_file_manager(&path);
-
-    #[cfg(target_os = "macos")]
-    {
-        let _ = std::process::Command::new("open")
-            .args(["-R", path.as_str()])
-            .spawn()
-            .unwrap();
-    }
+  #[cfg(not(target_os = "macos"))]
+  showfile::show_path_in_file_manager(&path);
+  
+  #[cfg(target_os = "macos")]
+  {
+    let _ = std::process::Command::new("open")
+      .args(["-R", path.as_str()])
+      .spawn()
+      .unwrap();
+  }
 }
 
 #[tauri::command]
 pub fn cmd_is_valid_path(path: String) -> bool {
-    let path = std::path::Path::new(&path);
-    path.exists()
+  let path = std::path::Path::new(&path);
+  path.exists()
 }
 
 #[tauri::command]
 pub fn cmd_is_valid_dir(path: String) -> bool {
-    let path = std::path::Path::new(&path);
-    path.exists() && path.is_dir()
+  let path = std::path::Path::new(&path);
+  path.exists() && path.is_dir()
 }
 
 #[tauri::command]
 pub fn cmd_is_valid_file(path: String) -> bool {
-    let path = std::path::Path::new(&path);
-    path.exists() && path.is_file()
+  let path = std::path::Path::new(&path);
+  path.exists() && path.is_file()
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![allow(dead_code)]
-#![allow(unused, unused_imports, unused_allocation, unused_assignments, unused_results, unused_variables)]
+#![allow(
+    unused,
+    unused_imports,
+    unused_allocation,
+    unused_assignments,
+    unused_results,
+    unused_variables
+)]
 
 use std::sync::Mutex;
 
@@ -11,13 +18,13 @@ mod app;
 mod cache;
 mod editor;
 mod errors;
+mod generate;
+mod git;
+mod io_utils;
 mod package;
 mod prefs;
 mod project;
-mod git;
-mod io_utils;
 mod template;
-mod generate;
 
 #[derive(Clone, serde::Serialize)]
 struct Payload {
@@ -26,83 +33,84 @@ struct Payload {
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            app::cmd_show_path_in_file_manager,
-            app::cmd_is_valid_path,
-            app::cmd_is_valid_dir,
-            app::cmd_is_valid_file,
-            // prefs
-            prefs::cmd_get_prefs,
-            prefs::cmd_load_prefs,
-            prefs::cmd_save_prefs,
-            prefs::cmd_set_pref_value,
-            prefs::cmd_set_prefs,
-            // user_cache
-            cache::cmd_get_user_cache,
-            cache::cmd_save_user_cache,
-            cache::cmd_set_user_cache_value,
-            cache::cmd_add_git_package_to_cache,
-            cache::cmd_add_local_package_to_cache,
-            cache::cmd_remove_git_package_from_cache,
-            cache::cmd_remove_local_package_from_cache,
-            cache::cmd_delete_template_cache,
-            // project
-            project::cmd_get_default_project_path,
-            project::cmd_remove_missing_projects,
-            project::cmd_add_project,
-            project::cmd_remove_project,
-            project::cmd_get_projects,
-            project::cmd_get_projects_on_page,
-            project::cmd_open_project_in_editor,
-            project::cmd_change_project_editor_version,
-            // editors
-            editor::cmd_get_editors,
-            editor::cmd_open_unity_hub,
-            editor::cmd_estimate_editor_size,
-            // packages
-            package::cmd_get_default_editor_packages,
-            // templates
-            template::cmd_get_surface_templates,
-            template::cmd_get_template_information,
-            template::cmd_get_template_file_paths,
-            template::cmd_delete_template,
-            // generate
-            generate::cmd_generate_project,
-            generate::cmd_generate_template,
-            // git
-            git::cmd_get_git_package_json
-        ])
-        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
-            println!("{}, {argv:?}, {cwd}", app.package_info().name);
-            app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
-        }))
-        .setup(|app| {
-            let app_handle = app.handle();
-            let prefs = app::load_prefs_from_disk(&app_handle)?;
-            let user_cache = app::load_user_cache_from_disk(&app_handle)?;
-            let projects = app::load_projects_from_disk(&app_handle)?;
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![
+      app::cmd_show_path_in_file_manager,
+      app::cmd_is_valid_path,
+      app::cmd_is_valid_dir,
+      app::cmd_is_valid_file,
+      // prefs
+      prefs::cmd_get_prefs,
+      prefs::cmd_load_prefs,
+      prefs::cmd_save_prefs,
+      prefs::cmd_set_pref_value,
+      prefs::cmd_set_prefs,
+      // user_cache
+      cache::cmd_get_user_cache,
+      cache::cmd_save_user_cache,
+      cache::cmd_set_user_cache_value,
+      cache::cmd_add_git_package_to_cache,
+      cache::cmd_add_local_package_to_cache,
+      cache::cmd_remove_git_package_from_cache,
+      cache::cmd_remove_local_package_from_cache,
+      cache::cmd_delete_template_cache,
+      // project
+      project::cmd_get_default_project_path,
+      project::cmd_remove_missing_projects,
+      project::cmd_add_project,
+      project::cmd_remove_project,
+      project::cmd_get_projects,
+      project::cmd_get_projects_on_page,
+      project::cmd_open_project_in_editor,
+      project::cmd_change_project_editor_version,
+      // editors
+      editor::cmd_get_editors,
+      editor::cmd_open_unity_hub,
+      editor::cmd_estimate_editor_size,
+      // packages
+      package::cmd_get_default_editor_packages,
+      // templates
+      template::cmd_get_surface_templates,
+      template::cmd_get_template_information,
+      template::cmd_get_template_file_paths,
+      template::cmd_delete_template,
+      // generate
+      generate::cmd_generate_project,
+      generate::cmd_generate_template,
+      // git
+      git::cmd_get_git_package_json
+    ])
+    .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+      println!("{}, {argv:?}, {cwd}", app.package_info().name);
+      app.emit_all("single-instance", Payload { args: argv, cwd })
+        .unwrap();
+    }))
+    .setup(|app| {
+      let app_handle = app.handle();
+      let prefs = app::load_prefs_from_disk(&app_handle)?;
+      let user_cache = app::load_user_cache_from_disk(&app_handle)?;
+      let projects = app::load_projects_from_disk(&app_handle)?;
 
-            app.manage(app::AppState {
-                prefs: Mutex::new(prefs),
-                user_cache: Mutex::new(user_cache),
-                projects: Mutex::new(projects),
-                editors: Mutex::new(Vec::new()),
-            });
+      app.manage(app::AppState {
+        prefs: Mutex::new(prefs),
+        user_cache: Mutex::new(user_cache),
+        projects: Mutex::new(projects),
+        editors: Mutex::new(Vec::new()),
+      });
 
-            let app_state = app.state::<app::AppState>();
-            match editor::find_editor_installs(&app_state) {
-                Ok(editors) => {
-                    (*app_state.editors.lock().unwrap()).clear();
-                    (*app_state.editors.lock().unwrap()).extend(editors);
-                },
-                Err(err) => {
-                    println!("{}", err);
-                }
-            }
+      let app_state = app.state::<app::AppState>();
+      match editor::find_editor_installs(&app_state) {
+        Ok(editors) => {
+          (*app_state.editors.lock().unwrap()).clear();
+          (*app_state.editors.lock().unwrap()).extend(editors);
+        }
+        Err(err) => {
+          println!("{}", err);
+        }
+      }
 
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
